5a6d219c6d914e90eb8063e2f0934e6755608f4c
Sławek Ludwiczak <slawek@javastart.pl>
2017-04-16 13:26:57 +0200

    zadanie-java-20

---
+++ src/FizzBuzz.java
@@ -0,0 +1,8 @@
+public class FizzBuzz {
+    public static void main(String[] args) {
+        for (int i = 0; i < 100; i++) {
+            if(i % 15 == 0)
+                System.out.println(i);
+        }
+    }
+}
